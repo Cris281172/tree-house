b2ffc7f1c6180cfc55fe6379d59cbdbfe0f62f61
Cris281172 <krzysiu.juczynski@gmail.com>
2021-09-26 19:47:00 +0200

    Delete animations

--- script.js
+++ script.js
@@ -20,25 +20,25 @@ const detectElementInViewport = el => {
     return rect.top >= 0 && rect.bottom <= window.innerHeight;
 }
 
-const slides = e => {
-    document.querySelectorAll('[data-animate]').forEach(element => {
-       if(detectElementInViewport(element)){
-           element.style.animationName = element.dataset.animate;
-           element.style.opacity = 1;
-       }
-    })
-}
+// const slides = e => {
+//     document.querySelectorAll('[data-animate]').forEach(element => {
+//        if(detectElementInViewport(element)){
+//            element.style.animationName = element.dataset.animate;
+//            element.style.opacity = 1;
+//        }
+//     })
+// }
 
-let timeout;
-
-
-window.addEventListener('scroll', () => {
-    showScroll();
-
-    clearTimeout(timeout);
-    timeout = setTimeout(slides, 100);
-    slides()
-});
+// let timeout;
+//
+//
+// window.addEventListener('scroll', () => {
+//     showScroll();
+//
+//     clearTimeout(timeout);
+//     timeout = setTimeout(slides, 100);
+//     slides()
+// });
 
 
 
